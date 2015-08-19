f644055fb7774b8d696f123328876bc33d8c986c
Matthias Thiel <mail@matthiasthiel.com>
2015-08-19 09:15:42 +0200

    extend item attributes

--- data/item-attribute.go
+++ data/item-attribute.go
@@ -1,13 +1,24 @@
 package data
 
 type ItemAttributes struct {
-	Binding   string
-	Brand     string
-	EAN       string
-	Languages Languages
-	ListPrice Price
-	Title     string
-	UPC       string
+	Authors         []string `xml:"Author"`
+	Binding         string
+	Brand           string
+	EAN             string
+	Edition         string
+	Features        []string `xml:"Features"`
+	ISBN            string
+	Label           string
+	Languages       Languages
+	ListPrice       Price
+	Manufacturer    string
+	ProductGroup    string
+	PublicationDate string
+	Publisher       string
+	ReleaseDate     string
+	Studio          string
+	Title           string
+	UPC             string
 }
 
 type Languages struct {
